b1008b396a16ff87a972c2580a17c31481c3f875
Lucas Penteado <38321808+lucaspsacchi@users.noreply.github.com>
2019-01-09 19:05:05 -0200

    Navbar do cadastro

---
+++ includes/nav-cad.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-md my-nav">
+    <div class="d-flex flex-row justify-content-between col-md-12">
+        <div class="d-flex col-2">
+            <a class="nav-item" href="home.php">
+                    <img src="../Imagens/Inter%20BCCS%20Logo%20Fundo%20Branco.png" width="50px" height="50px" alt="logo">
+            </a>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <h4>INTERBCCS</h4>
+        </div>
+
+        <div class="d-flex flex-row flex-nowrap col-2 justify-content-end">
+            <a class="nav-link" href="./perfil.php">PERFIL</a>
+            <a class="nav-link" href="./sair.php">SAIR</a>
+        </div>
+    </div>
+</nav>
